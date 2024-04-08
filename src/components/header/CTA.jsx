eb88f7a7e39a70e4f86bb15edbc911be22c08baf
lionelbalaji <97868425+lionelbalaji@users.noreply.github.com>
2024-04-08 19:25:46 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import RESUMEBALAJI from '../../assests/RESUMEBALAJI.pdf'
 
+const CTA=() => {
+  return (
+    <div className='cta'>
+       <a href={RESUMEBALAJI} download className='btn'>Download CV</a>
+       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
