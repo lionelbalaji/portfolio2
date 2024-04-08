93ac1bd6cdf78985437e990f95f41228647e3ab1
lionelbalaji <97868425+lionelbalaji@users.noreply.github.com>
2024-04-08 19:03:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import Nav from './components/nav/Nav';
+import Details from './components/details/Details';
+import About from './components/about/About';
+import Contact from  './components/contact/Contact'
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Details/>
+      <About/>
+      <Contact/>
+    </div>
+  )
+}
 
+export default App;
