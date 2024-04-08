5daf46a9d36d0165b9de25a0d59c89a846299a81
lionelbalaji <97868425+lionelbalaji@users.noreply.github.com>
2024-04-08 20:10:38 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import RESUMEBALAJI from '../../assests/RESUMEBALAJI.pdf'
-
+import CV2 from '../../assests/CV2.pdf'
 const CTA=() => {
   return (
     <div className='cta'>
-       <a href={RESUMEBALAJI} download className='btn'>Download CV</a>
+       <a href={CV2} download className='btn'>Download CV</a>
        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
