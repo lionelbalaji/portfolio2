cbec5cccc46a43f03944c4d2faedbfb9ac70e568
lionelbalaji <97868425+lionelbalaji@users.noreply.github.com>
2024-04-08 19:35:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+import  { createRoot } from "react-dom";
+import App from "./App";
+import './index.css'
+  
+const root=document.getElementById('root')
+const reactroot=createRoot(root)
+reactroot.render(<App/>)
+
 
